09a26a92735d934ce50c9065fe6dfffd28eb5252
Sebastian <styralski98@gmail.com>
2024-05-15 20:55:45 +0200

    map-update

--- eventHub/src/main.ts
+++ eventHub/src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router/router'
 import Toast, { POSITION } from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
+import 'leaflet/dist/leaflet.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 
 const app = createApp(App)
